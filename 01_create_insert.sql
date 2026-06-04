c22b74fc03a07b2282cabcabd7cee2fa3043215b
sanjana <125641551+Sanjana1812@users.noreply.github.com>
2026-06-04 14:08:23 -0400

    Update 01_create_insert.sql

--- 01_create_insert.sql
+++ 01_create_insert.sql
@@ -1,1 +1,16 @@
+CREATE DATABASE employee;
 
+\c employee
+
+CREATE TABLE employee(
+id SERIAL PRIMARY KEY,
+name VARCHAR(100),
+age INT
+);
+
+INSERT INTO employee(name,age)
+VALUES
+('san',22),
+('maggie',23);
+
+SELECT * FROM employee;
